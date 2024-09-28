03657fcac8bebf4239618ae12ba192c8ac8cc8ea
Chivier Humber <chivier.humber@outlook.com>
2024-09-28 22:52:35 +0800

    feat: update vectordb

--- quality_eval_1.py
+++ quality_eval_1.py
@@ -1,18 +1,58 @@
-from utils import *
+import json
 from datetime import datetime
 import random
 from nerif.nerif_core import *
-# Zero shot evaluation
+import numpy as np
 
-import torch
-from raptor import BaseSummarizationModel, BaseQAModel, BaseEmbeddingModel, RetrievalAugmentationConfig
-from transformers import AutoTokenizer, pipeline
+# Vector DB
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import PointStruct
+
+import platform
+
+if platform.system() == 'Linux':
+    from utils import *
+    import torch
+    from raptor import BaseSummarizationModel, BaseQAModel, BaseEmbeddingModel, RetrievalAugmentationConfig
+    from transformers import AutoTokenizer, pipeline
 
 question_file_count = 300
 question_position = "./parsed_data/quality_v1.0.1"
 
+def build_vectordb():
+    client = QdrantClient(url="http://localhost:6333")
+    vectordb_points = []
+    chunk_id = 0
+    for file_id in range(question_file_count):
+        print(f"File {file_id}")
+
+        article_chunk_text_path = f"{question_position}/article_{file_id}_chunks.txt"
+        article_chunk_embedding_path = f"{question_position}/article_{file_id}_chunks.npy"
+
+        article_chunks = []
+        with open(article_chunk_text_path, "r") as f:
+            for line in f:
+                article_chunks.append(line)
+        article_embeddings = np.load(article_chunk_embedding_path)
+        client.create_collection(
+            collection_name=f"quality_rag_article_{file_id}",
+            vectors_config=VectorParams(size=4096, distance=Distance.COSINE),
+        )
+        for i, chunk in enumerate(article_chunks):
+            chunk_id += 1
+            point = PointStruct(id=chunk_id, vector=article_embeddings[i], payload={"text": chunk})
+            vectordb_points.append(point)
+
+    client.upsert(
+        collection_name=f"quality_rag",
+        points=vectordb_points,
+    )
+    return client
+
+
 # Load questions
-def qa_test():
+def qa_test(client):
     question_count = 0
     correct_count = 0
     
@@ -20,30 +60,45 @@ def qa_test():
     log_filename = f"eval_1_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
     
     for file_id in range(question_file_count):
-        print(f"File {file_id}")
         file_path = f"{question_position}/question_{file_id}.jsonl"
         with open (file_path, "r") as f:
             for line in f:
                 data = json.loads(line)
                 original_question = data["question"]
                 correct_answer = data["answer"]
+
                 # find question in <QUESTION></QUESTION>
                 question = original_question.split("<QUESTION>")[1].split("</QUESTION>")[0]
-                print(question)
                 # find options in <OPTIONS></OPTIONS>
                 options = original_question.split("<OPTIONS>")[1].split("</OPTIONS>")[0].split("\n")
-                # parse options by newline  
                 options = [option.strip() for option in options]
-                print(options)
+                options = [option for option in options if option]
                 
-                selector = NerifMatchString(choices=options, model="gpt-4o-mini")
+                selector = NerifMatchString(choices=options, model="gpt-4o")
                 answer = selector.match(question)
+
+                # convert question to embedding
+                question_embedding = xxxx
+                # 
+                search_result = client.query_points(
+                    collection_name="quality_rag",
+                    query=question_embedding,
+                    with_payload=True,
+                    limit=3
+                ).points
+
+                print(search_result)
+                # article = 
+                # question_withrag = .... # add top k
+                # answer_withrag = selector.match(question_withrag)
+                
                 print(answer)
                 print(correct_answer)
-                exit(0)
+                break
                         
     print(f"Correct count: {correct_count}/{question_count}")
     print(f"Score: {correct_count/question_count}")
                 
 if "__main__" == __name__:
-    qa_test()
+    client = build_vectordb()
+    qa_test(client)
