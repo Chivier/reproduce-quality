d706a2ed19ddce2963330b2db1f73d59dcb75393
Chivier Humber <chivier.humber@outlook.com>
2024-09-28 22:24:15 +0800

    feat: rebuild with nerif

--- quality_eval_1.py
+++ quality_eval_1.py
@@ -1,6 +1,7 @@
 from utils import *
 from datetime import datetime
 import random
+from nerif.nerif_core import *
 # Zero shot evaluation
 
 import torch
@@ -10,19 +11,6 @@ from transformers import AutoTokenizer, pipeline
 question_file_count = 300
 question_position = "./parsed_data/quality_v1.0.1"
 
-common_heading = "You are a helpful assistant. You can help me by answering my questions.\n"
-common_prompt = "Give me the option that can answer this problem the best. Only give me the id of the best OPTION. If you cannot find the answer, give the option that you think is the best. ONLY RETURN THE ID!!!\n"
-
-def format_answer(answer):
-    answer = answer.strip().lower()
-    # answer is a number
-    if answer.isdigit():
-        return int(answer)
-    # if answer is a sentence, and there is only 1 number in the sentence
-    if len([int(s) for s in answer.split() if s.isdigit()]) == 1:
-        return int([int(s) for s in answer.split() if s.isdigit()][0])
-    return -1
-    
 # Load questions
 def qa_test():
     question_count = 0
@@ -36,43 +24,22 @@ def qa_test():
         file_path = f"{question_position}/question_{file_id}.jsonl"
         with open (file_path, "r") as f:
             for line in f:
-                get_answer = False
                 data = json.loads(line)
-                question = data["question"]
+                original_question = data["question"]
                 correct_answer = data["answer"]
-                question = common_heading + question + common_prompt
-                
-                max_retries = 10
-                temperature = 0
-                # while not get_answer:
-                #     answer = get_ollama_response(question, temperature=temperature)
-                #     formated_answer = format_answer(answer)
-                #     if formated_answer == -1:
-                #         print("Invalid answer")
-                #         print(f"Answer: {answer}")
-                #         max_retries -= 1
-                #         if max_retries == 0:
-                #             print("Max retries reached")
-                #             # use random answer
-                #             # count options between <OPTIONS></OPPTIONS> in quesiton
-                #             options = question.split("<OPTIONS>")[1].split("</OPTIONS>")[0].split("\n")
-                #             options_count = len(options)
-                #             formated_answer = random.randint(0, options_count-1)
-                #             get_answer = True
-                #             question_count += 1
-                #             if formated_answer == correct_answer:
-                #                 correct_count += 1
-                #         temperature += 0.1
-                #         continue
-                #     else:
-                #         get_answer = True
-                #         question_count += 1
-                #         if formated_answer == correct_answer:
-                #             correct_count += 1
-                #         log_file = open(log_filename, "a")
-                #         log_file.write(f"Eval_answer: {answer}, Correct_answer: {correct_answer}\n")
-                #         log_file.close()
+                # find question in <QUESTION></QUESTION>
+                question = data["question"].split("<QUESTION>")[1].split("</QUESTION>")[0]
                 print(question)
+                # find options in <OPTIONS></OPTIONS>
+                options = question.split("<OPTIONS>")[1].split("</OPTIONS>")[0].split("\n")
+                # parse options by newline  
+                options = [option.strip() for option in options]
+                print(options)
+                
+                selector = NerifMatchString(choices=options, model="gpt-4o-mini")
+                answer = selector.match(question)
+                print(answer)
+                print(correct_answer)
                 exit(0)
                         
     print(f"Correct count: {correct_count}/{question_count}")
